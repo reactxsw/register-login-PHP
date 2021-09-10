c87d2536ea4c4646147f6b492c282ea96fa98c6f
Anapah <70201574+reactxsw@users.noreply.github.com>
2021-09-11 04:06:53 +0700

    Update register_db.php

--- register_db.php
+++ register_db.php
@@ -4,103 +4,50 @@
     
     $errors = array();
 
-
-    if (isset($_POST['register'])) {
+    if (isset($_POST['setting'])) {
         unset($_SESSION['error']);
-        $username = mysqli_real_escape_string($connect, $_POST['username']);
-        $email = mysqli_real_escape_string($connect, $_POST['email']);
-        $password = mysqli_real_escape_string($connect, $_POST['password']);
-        $password_cornfirm = mysqli_real_escape_string($connect, $_POST['password_confirm']);
+        $username = $_SESSION['username'];
+        $displayname = mysqli_real_escape_string($connect, $_POST['displayname']);
 
-        if(isset($_POST['h-captcha-response']) && !empty($_POST['h-captcha-response'])){
-            $data = array(
-                'secret' => "0xEe6aE2ae3931Bd7b6FA2a17490EF89f632fF587D",
-                'response' => $_POST['h-captcha-response']
-            );
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, "https://hcaptcha.com/siteverify");
-            curl_setopt($ch, CURLOPT_POST, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $response = curl_exec($ch);
-            $responseData = json_decode($response);
-            if(!$responseData->success) {
-                array_push($errors, "verification failed, please try again");
-                $_SESSION['error'] = "verification failed, please try again";
-            }   
-        } else {
-            array_push($errors, "Captcha is required");
-            $_SESSION['error'] = "Captcha is required";
+        $query = mysqli_query($connect, "SELECT * FROM user WHERE displayname = '$displayname' LIMIT 1");
+        $result = mysqli_fetch_assoc($query);
+        if (($result) && ($_SESSION["displayname"] != $displayname)) {
+            array_push($errors, "name already exists");
+            $_SESSION['error'] = "name already exists";
+            echo "saved";
+
         }
+        $directory = "assets/img/profile/";
+        $filename = 'm'.$_SESSION["id"].'.png';
+        $target_file = $directory.$filename; 
 
-        if (empty($password)) {
-            array_push($errors, "Password is required");
-            $_SESSION['error'] = "Password is required";
-        } else {
-            if (strlen($password) < 8) {
-                array_push($errors, "Password must be atleast 8 letters");
-                $_SESSION['error'] = "Password must be atleast 8 letters";
-            }
-        }
-
-        if (empty($password_cornfirm)) {
-            array_push($errors, "Confirm your password");
-            $_SESSION['error'] = "Confirm your password";
-        }
-
-        if (!empty($password_cornfirm && $password != $password_cornfirm)) {
-            array_push($errors, "The two passwords do not match");
-            $_SESSION['error'] = "The two passwords do not match";
-        }
-
-        if (empty($email)) {
-            array_push($errors, "Email is required");
-            $_SESSION['error'] = "Email is required";
-
-        } else {
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                array_push($errors, "Invalid email");
-                $_SESSION['error'] = "Invalid email";
-              } else {
-                $query = mysqli_query($connect, "SELECT * FROM user WHERE email = '$email' LIMIT 1");
-                $result = mysqli_fetch_assoc($query);
-                if ($result) {
-                    array_push($errors, "email already exists");
-                    $_SESSION['error'] = "email already exists";
-    
-                }
-              }
-        }
+        if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+            $resize = new ResizeImage($target_file);
+            $resize->resizeTo(125, 125, 'exact');
+            $resize->saveImage($filename);
         
-        if (empty($username)) {
-            array_push($errors, "Username is required");
-            $_SESSION['error'] = "Username is required";
-
         } else {
-            $query = mysqli_query($connect, "SELECT * FROM user WHERE username = '$username' LIMIT 1");
-            $result = mysqli_fetch_assoc($query);
-            if ($result) {
-                array_push($errors, "Username already exists");
-                $_SESSION['error'] = "Username already exists";
-
-            }
+            array_push($errors, "Unable to upload image");
+            $_SESSION['error'] = "Unable to upload image";
         }
 
-        if (!count($errors) > 0) {
+        if (count($errors) == 0) {
+            echo "saved";
             $password = md5($password);
-
-            $sql = "INSERT INTO user (username, displayname, email, password) VALUES ('$username', '$username', '$email', '$password')";
-            if ($sql) {
-                mysqli_query($connect, $sql);
-
-                $_SESSION['username'] = $username;
+            $sql = "UPDATE user SET displayname='$displayname' WHERE username='$username'";
+            if (mysqli_query($connect, $sql)) {
+                echo "Record updated successfully";
+                $_SESSION['displayname'] = $displayname;
                 $_SESSION['success'] = "You are now logged in";
-                header('location: index.php');
-
+                header('location: setting.php');
+            } else {
+                echo "Error updating record: " . mysqli_error($conn);
+                header('location: setting.php');
             }
-
+            
         } else {
-            header("location: register.php");
+            header("location: setting.php");
         }
     }
+
 ?>
