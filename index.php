453fa3c99dda32a05f449e77d3235ac51491ccdd
Anapah <70201574+reactxsw@users.noreply.github.com>
2021-09-12 21:47:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,14 +2,13 @@
     session_start();
     include('server.php'); 
 
-    if (!isset($_SESSION['username'])) {
-        $_SESSION['msg'] = "You must log in first";
+    if (!isset($_SESSION["username"])) {
         header('location: login.php');
     }
 
     if (isset($_GET['logout'])) {
         session_destroy();
-        unset($_SESSION['username']);
+        unset($_SESSION["username"]);
         header('location: login.php');
     }
 
@@ -31,19 +30,21 @@
     </div>
 
     <div class="homecontent">
-        <?php if (isset($_SESSION['success'])) : ?>
-            <div class="success">
-                <h3>
-                    <?php 
-                        echo $_SESSION['success'];
-                        unset($_SESSION['success']);
-                    ?>
-                </h3>
-            </div>
-        <?php endif ?>
-    
-        <?php if (isset($_SESSION['username'])) : ?>
-            <p>Welcome   <?php echo $_SESSION['username']; ?></p>
+        <?php
+            if ($_SESSION["profile"] == "blank"){
+                $img_path = 'assets/img/blank.png';
+
+            } else {
+                $filename = 'm'.$_SESSION["id"];
+                $img_path = 'assets/img/profile/'.$filename.'.png';
+            }
+        ?>    
+
+        <?php if (isset($_SESSION['displayname'])) : ?>
+            <img src="<?php echo $img_path; ?>" style="width:125px; height:125px;"/>
+            <p>Welcome   <?php echo $_SESSION["displayname"]; ?></p>
+            <p>Welcome   <?php echo $_SESSION["id"]; ?></p>
+            <p>Welcome   <?php echo $_SESSION["profile"]; ?></p>
             <p><a href="index.php?logout='1'" style="color: red;">Logout</a></p>
             <p><a href="setting.php" style="color: black;">Setting</a></p>
 
